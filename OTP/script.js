ef7e80663d9f82671b58a885cda230946abee740
bhaviljain <bhaviljain26@gmail.com>
2024-03-05 00:53:37 +0530

    OTP INPUT

---
+++ OTP/script.js
@@ -0,0 +1,30 @@
+let typedInputs = document.getElementById('inputs')
+
+typedInputs.addEventListener("input", function(e){
+ const type = e.target
+ const val = type.value
+
+ if(isNaN(val))
+ {
+    type.value =''
+    return;
+ }
+ if(val)
+ {
+    const next = type.nextElementSibling;
+    if(next)
+    next.focus()
+ }
+})
+typedInputs.addEventListener('keyup', function(e){
+    const typedDelete = e.target
+    const keyTyped = e.key.toLocaleLowerCase()
+
+    if(keyTyped === "backspace" || keyTyped === "delete")
+    {
+        typedDelete.value =''
+        const prev = typedDelete.previousElementSibling;
+        prev.focus()
+    }
+
+})
