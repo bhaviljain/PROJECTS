76333bafc5bfb79daf65b1d24a5e50e9b17bb5ee
bhaviljain <bhaviljain26@gmail.com>
2024-03-07 01:38:44 +0530

    accordian

--- Accordian/acc.js
+++ Accordian/acc.js
@@ -5,17 +5,19 @@ let show = document.getElementsByClassName('show')
 for(let i = 0; i <item.length; i++)
 {
     item[i].addEventListener('click', ()=>{
-        let result = item[i].classList.toggle("active")
+        let result = item[i].classList.toggle("act")
         if(result === true)
         {
             hide[i].style.display = "block"
             hide[i].style.color = "black"
             show[i].style.display = "none"
-        }
+            console.log("active");
+        }   
         else
         {
             hide[i].style.display = "none"
             show[i].style.display = "block"
+            console.log('unactive');
         }
     })
 }
